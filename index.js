1cd51caa6adf2aeacfddfaf23a89091a8cc31fc5
Crazydriver8 <krazydriver8@gmail.com>
2016-01-04 00:08:16 -0500

    basic front end setup
    ayyayaysfydaisdfu

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var Express = require('express');
 var app = Express();
 
 var http = require('http').Server(app),
-	port = 1337,
-	debug = require(debug)('http');
+	port = 1337;
+	debug = require('debug')('http');
 
-var modules = require('./modules/modules.js')();
+//var modules = require('./modules/modules.js')();
 
 app.use(Express.static('pages'));
 
 app.get('/', function(req,res) {
-	console.log(modules.blog.b);
-	res.sendFile(__dirname + '/pages/test.html');
+	//console.log(modules.blog.b);
+	res.sendFile(__dirname + '/pages/main/test.html');
 });
 
 
 http.listen(port, function() {
-	debug('listening on port *:port');
+	debug('Listening on port *:' + port);
 });
